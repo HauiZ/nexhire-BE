27f0faf8400991a94f4affdf071f7761ef30ec95
HauiZ <hauiz15122004@gmail.com>
2026-07-16 14:19:27 +0700

    fix notification mailer adapter import

--- apps/notification-service/src/notification-service.module.ts
+++ apps/notification-service/src/notification-service.module.ts
@@ -1,6 +1,6 @@
 import { join } from 'node:path';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/adapters/handlebars.adapter';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
