c37ffc2b3daa6e9756f413ca47ed9ff03aa406b3
HauiZ <hauiz15122004@gmail.com>
2026-07-16 11:26:29 +0700

    style(company): format company docs and migration

--- apps/company-service/src/migrations/1783786890301-CreateCompaniesTable.ts
+++ apps/company-service/src/migrations/1783786890301-CreateCompaniesTable.ts
@@ -11,8 +11,12 @@ export class CreateCompaniesTable1783786890301 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "companies" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE, "name" character varying(255) NOT NULL, "logo" character varying, "description" text, "website" character varying, "address" character varying, "tax_code" character varying(50) NOT NULL, "owner_id" uuid NOT NULL, "status" "public"."companies_status_enum" NOT NULL DEFAULT 'PENDING', CONSTRAINT "pk_companies_id" PRIMARY KEY ("id"))`,
     );
-    await queryRunner.query(`CREATE UNIQUE INDEX "uq_company_tax_code" ON "companies" ("tax_code")`);
-    await queryRunner.query(`CREATE UNIQUE INDEX "uq_company_owner_id" ON "companies" ("owner_id")`);
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "uq_company_tax_code" ON "companies" ("tax_code")`,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "uq_company_owner_id" ON "companies" ("owner_id")`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
